6983a794192bfa4fa84f1640538255ebe09a203f
Devin Wulf <devinlwulf@gmail.com>
2022-03-26 17:01:28 -0500

    file structuring

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-// const util = require('util')
 const fs = require('fs');
+const inquirer = require('inquirer');
 const generateReadme = require('./utils/generateMarkdown')
-// const createFile = util.promisify(fs.writeFile)
 
-
+// TODO: Create a function to initialize app
 function init() {
   inquirer
     .prompt([
@@ -89,16 +87,5 @@ function init() {
       console.log('README successfully created.');
   });
 }
-
-// TODO: Create a function to initialize app
-// async function init() {
-//   try {
-//     const response = await userInput();
-//     console.log(response);
-//     await createFile('README.md', generateReadme(response));
-//   } catch(err) {
-//     console.error(err);
-//   }
-// }
   // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,3 @@
-// // TODO: Create a function that returns a license badge based on which license is passed in
-// // If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
-
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
-
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
   return `
@@ -23,12 +11,12 @@ ${response.description}
 
 ## Table of Contents
 
-[Installation](#installation)
-[Usage](#usage)
-[Contributions](#contributions)
-[Testing](#testing)
-[License](#license)
-[Questions](#questions)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributions](#contributions)
+- [Testing](#testing)
+- [License](#license)
+- [Questions](#questions)
 
 ## Installation Instructions
 
